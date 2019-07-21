37f3f0782de9217adac0aa97d52f7228bfb8548a
Kris Kamikawa <kris.kamikawa@gmail.com>
2019-07-21 16:24:06 -0600

    function that bubble sorts array of numbers

---
+++ sortingAlgorithm/build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+//bubble sort array of numbers
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
---
+++ sortingAlgorithm/src/index.ts
@@ -0,0 +1,11 @@
+//bubble sort array of numbers
+
+class Sorter {
+  collection: number[];
+
+  constructor(collection: number[]) {
+    this.collection = collection;
+  }
+}
+
+const sorter = new Sorter ([10, 3, -5, 0])
