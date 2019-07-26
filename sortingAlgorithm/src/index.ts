43006d538ab6580c5828cc734e38ebcb88d04a38
Kris Kamikawa <kris.kamikawa@gmail.com>
2019-07-26 17:22:37 -0600

    add linked list class

--- sortingAlgorithm/src/index.ts
+++ sortingAlgorithm/src/index.ts
@@ -1,9 +1,20 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 
-const charactersCollection = new CharactersCollection("Xaaayb");
-const sorter = new Sorter(charactersCollection);
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
+
+// const charactersCollection = new CharactersCollection("Xaaayb");
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
